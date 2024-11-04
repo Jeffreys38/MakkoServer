e08426408a539235d00846ba2e2ba0bfd41561e9
Nevi38 <quocthangngoc14@gmail.com>
2024-11-04 16:02:36 +0700

    feat(test): add ResetPasswordMail, WelcomeMail, OtpMail route testing module

---
+++ src/route/__tests__/MailRoute/OtpMail.test.ts
@@ -0,0 +1,63 @@
+import { Request, Response } from 'express';
+import mailController from '../../../controller/MailController';
+import Makko from '../../../Makko';
+
+jest.mock('../../../Makko', () => ({
+    __esModule: true,
+    default: {
+        getConfig: jest.fn().mockReturnValue({
+            mailer: {
+                sendMail: jest.fn(),
+            },
+            firebase: {
+                storage: 'mockStorageInstance',
+            },
+        }),
+    },
+}));
+
+jest.mock('../../../app/mail/OtpVerificationEmail');
+
+describe('Otp Verification Email', () => {
+    let req: Partial<Request>;
+    let res: Partial<Response>;
+    let sendMailSpy: jest.SpyInstance;
+
+    beforeEach(() => {
+        req = {
+            body: {
+                email: 'quocthangngoc14@gmail.com',
+                receiverName: 'Test User 1'
+            }
+        };
+
+        res = {
+            status: jest.fn().mockReturnThis(),
+            json: jest.fn()
+        };
+
+        sendMailSpy = jest.spyOn(Makko.getConfig().mailer, 'sendMail');
+    });
+
+    afterEach(() => {
+        jest.clearAllMocks();
+    });
+
+    it('should respond with code 200 when OTP email is sent successfully', async () => {
+        sendMailSpy.mockResolvedValueOnce(undefined); // Giả lập thành công
+
+        await mailController.sendOtpMail(req as Request, res as Response);
+
+        expect(res.status).toHaveBeenCalledWith(200);
+        expect(res.json).toHaveBeenCalledWith({ success: true, message: "OTP email sent" });
+    });
+
+    it('should respond with code 500 when sendMail throws an error', async () => {
+        sendMailSpy.mockRejectedValueOnce(new Error('Failed to send email')); // Giả lập lỗi
+
+        await mailController.sendOtpMail(req as Request, res as Response);
+
+        expect(res.status).toHaveBeenCalledWith(500);
+        expect(res.json).toHaveBeenCalledWith({ success: false, message: 'Failed to send email' });
+    });
+});
